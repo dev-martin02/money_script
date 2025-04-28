716c553ac39dc1ae22a50de477bfff8f67168a4b
Web-Martin <martin052017@outlook.com>
2025-04-27 20:28:40 -0400

    new strcture of folder + start of new feature budgeting

---
+++ modules/budgeting/budgeting.js
@@ -0,0 +1,74 @@
+import { available_money_monthly } from "../../utils/helpers.js";
+import { pdf_maker } from "../../utils/pdf-maker.js";
+import { month_planner } from "./planning/planner.js";
+
+const user_expenses = [
+  {
+    name: "gym membership",
+    cost: 30,
+    frequency: "monthly",
+    dueDate: 3,
+  },
+  {
+    name: "electric bill",
+    cost: 120,
+    frequency: "monthly",
+    dueDate: 12,
+  },
+  {
+    name: "netflix",
+    cost: 15,
+    frequency: "monthly",
+    dueDate: 18,
+  },
+  {
+    name: "groceries",
+    cost: 100,
+    frequency: "weekly",
+    dueDate: "saturday",
+  },
+  {
+    name: "laundry",
+    cost: 25,
+    frequency: "bi-weekly",
+    dueDate: "tuesday",
+  },
+  {
+    name: "amazon prime",
+    cost: 14,
+    frequency: "monthly",
+    dueDate: 25,
+  },
+  {
+    name: "student loan",
+    cost: 200,
+    frequency: "monthly",
+    dueDate: 28,
+  },
+  {
+    name: "water bill",
+    cost: 50,
+    frequency: "monthly",
+    dueDate: 10,
+  },
+  {
+    name: "apple music",
+    cost: 11,
+    frequency: "monthly",
+    dueDate: 22,
+  },
+];
+const user_paycheck = 1000;
+
+function create_budgeting_plan(params) {
+  // PARAMS -> EXPENSES LIST, USER INCOME (PAY CHECK)
+  // user_expenses -> list of json array
+  const month_plan = month_planner(user_expenses);
+  console.log(month_plan);
+
+  const money_left = available_money_monthly(user_expenses, user_paycheck);
+  console.log(money_left);
+  pdf_maker(month_plan, money_left);
+}
+
+create_budgeting_plan();
---
+++ modules/budgeting/planning/planner.js
@@ -0,0 +1,92 @@
+import {
+  find_day,
+  month_dates,
+} from "../../../utils/month-calculation/month.js";
+
+// Makes a map of all your expenses for the month
+export function month_planner(user_expenses) {
+  const today = new Date();
+  const current_month = today.getMonth();
+  const current_year = today.getFullYear();
+
+  const current_month_info = month_dates(current_year, current_month);
+
+  const planner_month = create_planner(current_month_info);
+
+  const user_payment_month = {};
+
+  // modify the obj, containing a map of all the days the user will have to pay his bills
+  user_expenses.forEach((expenses) => {
+    if (expenses.frequency === "monthly") {
+      const day_to_find = find_day(expenses.dueDate, planner_month);
+
+      if (user_payment_month[day_to_find[0]] === undefined) {
+        user_payment_month[day_to_find[0]] = [];
+      }
+
+      const monthly_Obj = {
+        name: expenses.name,
+        cost: expenses.cost,
+        dueDate: expenses.dueDate,
+      };
+
+      user_payment_month[day_to_find[0]].push(monthly_Obj);
+    }
+
+    if (expenses.frequency === "weekly") {
+      for (let i = 0; i < 5; i++) {
+        if (user_payment_month[i] === undefined) {
+          user_payment_month[i] = [];
+        }
+        const monthly_Obj = {
+          name: expenses.name,
+          cost: expenses.cost,
+          dueDate: expenses.dueDate,
+        };
+
+        user_payment_month[i].push(monthly_Obj);
+      }
+    }
+
+    if (expenses.frequency === "bi-weekly") {
+      const day_to_find = find_day(expenses.dueDate, planner_month);
+      if (user_payment_month[day_to_find[0]] === undefined) {
+        user_payment_month[day_to_find[0]] = [];
+      } else if (user_payment_month[day_to_find[0] + 2] === undefined) {
+        user_payment_month[day_to_find[0] + 2] = [];
+      }
+
+      const monthly_Obj = {
+        name: expenses.name,
+        cost: expenses.cost,
+        dueDate: expenses.dueDate,
+      };
+      user_payment_month[day_to_find[0]].push(monthly_Obj);
+
+      user_payment_month[day_to_find[0] + 2].push(monthly_Obj);
+    }
+  });
+
+  return user_payment_month;
+}
+
+// Return an Obj with the days of the months breakdown in weeks
+export function create_planner(month_info) {
+  const weeks = month_info.length / 7;
+  let last_week_day = 0;
+  let first_week_day = 0;
+
+  let weeks_in_month = {};
+
+  for (let i = 0; i < weeks; i++) {
+    last_week_day += 7;
+    if (first_week_day === 0) {
+      weeks_in_month[i] = month_info.slice(first_week_day, last_week_day);
+      first_week_day += 7;
+    } else {
+      weeks_in_month[i] = month_info.slice(first_week_day, last_week_day);
+      first_week_day += 7;
+    }
+  }
+  return weeks_in_month;
+}
